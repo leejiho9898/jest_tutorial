5616638695dedb2743b46c47e6449305320df93c
이지호 <cctv1998@naver.com>
2021-12-07 18:30:28 +0900

    Test : react-test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,57 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //screem object를 이용해서 원하는 엘레멘트에 접근
+  const counterElement = screen.getByTestId("counter");
 
-  const lintTest = screen.getByRole("button", {
-    name: "lintTest",
-  });
+  //id가 counter인 엘레멘트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
 });
 
-expect(lintTest.textContext).toBe("lintTest");
+test("minus button has correct text", () => {
+  render(<App />);
+  //screem object를 이용해서 원하는 엘레멘트에 접근
+  const btnEle = screen.getByTestId("minusBtn");
+
+  //id가 minusBtn인 엘레멘트의 텍스트가 0인지 테스트
+  expect(btnEle).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  //위와동일
+  const btnEle = screen.getByTestId("plusBtn");
+
+  expect(btnEle).toHaveTextContent("+");
+});
+
+test("wnen plus button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  //위와동일
+  const btnEle = screen.getByTestId("plusBtn");
+  fireEvent.click(btnEle);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("wnen minus button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  //위와동일
+  const btnEle = screen.getByTestId("minusBtn");
+  fireEvent.click(btnEle);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test("Prevent the -,+ btn from being pressed when theon/off button is clicked", () => {
+  render(<App />);
+  //screem object를 이용해서 원하는 엘레멘트에 접근
+  const Element = screen.getByTestId("onOffBtn");
+  fireEvent.click(Element);
+
+  const btnMEle = screen.getByTestId("minusBtn");
+  const btnPEle = screen.getByTestId("plusBtn");
+  expect(btnMEle,).toBeDisabled();
+});
